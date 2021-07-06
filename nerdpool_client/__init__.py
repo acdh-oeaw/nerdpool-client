12d383d336db275d3c6f909d897be8c3cea48ef8
csae8092 <peter.andorfer@oeaw.ac.at>
2021-07-06 10:12:55 +0200

    dump_to_jsonl() added

--- nerdpool_client/__init__.py
+++ nerdpool_client/__init__.py
@@ -1,3 +1,4 @@
+import json
 import requests
 
 
@@ -5,6 +6,9 @@ NP_BASE_URL = "https://nerdpool-api.acdh-dev.oeaw.ac.at/api/"
 
 
 class NerdPoolClient():
+    """
+    Class to interact with nerdpool-api endpoint
+    """
 
     def __init__(self, base_url=NP_BASE_URL):
         self.base_url = base_url
@@ -26,32 +30,42 @@ class NerdPoolClient():
 
     def list_data_set_titles(self):
         return [x['title'] for x in self.fetch_data_sets()]
-    
-    def yield_samples(self, dataset_title=False, limit=False, **kwargs):
+
+    def sample_to_json(self, sample):
+
+        """
+        takes a ner-sample as it comes from nerdpool-api and returns a spacy conform dict
+        :param sample: ner-sample as it comes from nerdpool-api
+
+        :returns: a dict like `{"text": "Wien ist groß.", "entities": [0, 3, "LOC"]}
+        :rtype: dict
+        """
+
+        ner_sample = sample['ner_sample']
+        item = {}
+        item['text'] = ner_sample['text']
+        item['entities'] = ner_sample['entities']
+        return item
+
+    def yield_samples(self, url, limit=False):
+
         """ iterator to yield all ner-samples
-        :param dataset_title: Title of a specific dataset
+
+        :param url: The url of the data to fetch
         :type dataset_title: str
         :param limit: Bool to flag if only a short sample\
         of samples should be fetched, defaults to `False`
         :type limit: bool
+
         :return: An iterator yielding abbreviations
         :rtype: iterator
         """
-        params = []
-        print(kwargs)
         next = True
-        if dataset_title:
-            params.append(('ner_source__title', dataset_title))
-        if kwargs:
-            for key, value in kwargs.items():
-                params.append((key, value))
-        
-        url = self.sample_ep
         counter = 0
         if limit:
             max_samples = 5
         while next:
-            response = requests.get(url, params=params)
+            response = requests.get(url)
             print(response.url)
             result = response.json()
             if result.get('next', False):
@@ -67,3 +81,26 @@ class NerdPoolClient():
                         yield(x)
                 else:
                     yield(x)
+
+    def dump_to_jsonl(self, url, limit=False, file_name="out.jsonl"):
+
+        """
+        dumps the data from the given URL to a jsonl file
+
+        :param url: The url of the data to fetch
+        :type dataset_title: str
+        :param limit: Bool to flag if only a short sample\
+        of samples should be fetched, defaults to `False`
+        :type limit: bool
+        :param file_name: the name of the file to save the dump
+        :type file_name: string
+
+        :return: The file name
+        :rtype: str
+        """
+        with open(file_name, "w") as out_file:
+            for x in self.yield_samples(url, limit=limit):
+                item = self.sample_to_json(x)
+                out_file.write(json.dumps(item, ensure_ascii=False))
+                out_file.write("\n")
+        return file_name
