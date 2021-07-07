16c0b6f32923cb001563ed6e7434739e20da3101
csae8092 <peter.andorfer@oeaw.ac.at>
2021-07-07 11:45:18 +0200

    more tests

--- nerdpool_client/test/test_npclient.py
+++ nerdpool_client/test/test_npclient.py
@@ -1,4 +1,8 @@
 import json
+import types
+import os
+
+
 from unittest import TestCase
 
 from nerdpool_client import NerdPoolClient
@@ -17,6 +21,8 @@ SAMPLE_JSON = json.loads("""{
     "ner_ent_type": "PER",
     "ner_source": "https://nerdpool-api.acdh-dev.oeaw.ac.at/api/ner-source/9/?format=json"
 }""")
+ENDPOINT = "https://nerdpool-api.acdh-dev.oeaw.ac.at/api/ner-sample"
+JSONL_DUMP = "hansi4ever_out.jsonl"
 
 
 class TestNerdPoolClient(TestCase):
@@ -31,3 +37,20 @@ class TestNerdPoolClient(TestCase):
     def test_sample_to_json(self):
         item = client.sample_to_json(SAMPLE_JSON)
         self.assertEqual(len(item.keys()), 2)
+
+    def test_yield_samples(self):
+        data = client.yield_samples(ENDPOINT, limit=True)
+        self.assertTrue(isinstance(data, types.GeneratorType))
+        self.assertTrue(len([x for x in data]), 5)
+
+    def test_dump_to_jsonl(self):
+        try:
+            os.remove(JSONL_DUMP)
+        except FileNotFoundError:
+            pass
+        dump = client.dump_to_jsonl(ENDPOINT,  limit=True, file_name=JSONL_DUMP)
+        self.assertEqual(dump, JSONL_DUMP)
+        with open(JSONL_DUMP, "r") as f:
+            sample = json.loads(f.readline())
+            self.assertTrue('text' in sample.keys())
+        os.remove(JSONL_DUMP)
