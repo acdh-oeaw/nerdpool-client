b45e4a8f0d733c1079c35c6b2f93f7c8727566d6
csae8092 <peter.andorfer@oeaw.ac.at>
2021-07-07 11:21:30 +0200

    test for sample_to_json added

--- nerdpool_client/test/test_npclient.py
+++ nerdpool_client/test/test_npclient.py
@@ -1,11 +1,33 @@
+import json
 from unittest import TestCase
 
 from nerdpool_client import NerdPoolClient
+
+client = NerdPoolClient()
+SAMPLE_JSON = json.loads("""{
+    "url": "https://nerdpool-api.acdh-dev.oeaw.ac.at/api/ner-sample/55084/?format=json",
+    "ner_text": "Votant könne daher nur dem Antrage des Staatsratspräsidenten beipflichten.",
+    "ner_sample": {
+        "text": "Votant könne daher nur dem Antrage des Staatsratspräsidenten beipflichten.",
+        "entities": [
+            [39, 60, "PER"]
+        ]
+    },
+    "ner_ent_exist": true,
+    "ner_ent_type": "PER",
+    "ner_source": "https://nerdpool-api.acdh-dev.oeaw.ac.at/api/ner-source/9/?format=json"
+}""")
 
 
 class TestNerdPoolClient(TestCase):
 
     def test_data_sets(self):
-        client = NerdPoolClient()
         data_sets = client.data_sets
         self.assertTrue(isinstance(data_sets, list))
+
+    def test_str(self):
+        self.assertEqual(f"{client}", "Nerdpool API Client")
+
+    def test_sample_to_json(self):
+        item = client.sample_to_json(SAMPLE_JSON)
+        self.assertEqual(len(item.keys()), 2)
