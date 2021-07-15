c6ade110859508e4362945807534cc9cb3af8246
csae8092 <peter.andorfer@oeaw.ac.at>
2021-07-15 13:43:27 +0200

    long_description added

--- setup.py
+++ setup.py
@@ -1,10 +1,16 @@
 import setuptools
+from os import path
 
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setuptools.setup(
     name="nerdpool-client",
     version="0.0.1",
     author="Peter Andorfer",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     author_email="peter.andorfer@oeaw.ac.at",
     description="A client for Nerdpool-Api",
     url="https://github.com/acdh-oeaw/nerdpool-client",
