d39ecfe186d9cdc3c3d4cbb24c8572a4853c9037
csae8092 <peter.andorfer@oeaw.ac.at>
2021-07-15 13:44:08 +0200

    version bump

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setuptools.setup(
     name="nerdpool-client",
-    version="0.0.1",
+    version="1.0.0",
     author="Peter Andorfer",
     long_description=long_description,
     long_description_content_type='text/markdown',
